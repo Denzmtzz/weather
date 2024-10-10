b94ca5ae758f517e9ae507a1ee2c71c9edd5ea29
Denzmtzz <jarendenzel@gmail.com>
2024-10-10 10:16:25 -0700

    weather.py

--- weather.py
+++ weather.py
@@ -3,8 +3,8 @@ import requests
 API_KEY = '0508b5e6db9b6fb4dea69718840'
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
 
-city = input("Sacramento: ")
-request_url = f"{BASE_URL}q={Sacramento}&appid={0508b5e6db9b6fb4dea69718840}&units=metric"
+city = input("Enter city name: ")
+request_url = f"{BASE_URL}q={city}&appid={0508b5e6db9b6fb4dea69718840}&units=metric"
 response = requests.get(request_url)
 
 if response.status_code == 200:
